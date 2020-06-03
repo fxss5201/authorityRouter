6e33bd09dd8c8462e37ef6975219976110c74fc2
fxss <723107234@qq.com>
2020-06-03 15:11:06 +0800

    perf: :zap:将Vue、axios、element-ui、vue-router、vuex采用CDN引入的方式

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: '/authorityRouter/',
+  publicPath: '/authorityRouterCdn/',
   devServer: {
     proxy: {
       '/api': {
@@ -7,6 +7,41 @@ module.exports = {
         ws: true,
         changeOrigin: true
       }
+    }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      var externals = {
+        vue: 'Vue',
+        axios: 'axios',
+        'element-ui': 'ELEMENT',
+        'vue-router': 'VueRouter',
+        vuex: 'Vuex'
+      }
+      config.externals(externals)
+      const cdn = {
+        css: [
+          // element-ui css
+          'https://cdn.bootcdn.net/ajax/libs/element-ui/2.4.5/theme-chalk/index.css'
+        ],
+        js: [
+          // vue
+          'https://cdn.bootcdn.net/ajax/libs/vue/2.6.11/vue.min.js',
+          // vue-router
+          'https://cdn.bootcdn.net/ajax/libs/vue-router/3.1.5/vue-router.min.js',
+          // vuex
+          'https://cdn.bootcdn.net/ajax/libs/vuex/3.1.2/vuex.min.js',
+          // axios
+          'https://cdn.bootcdn.net/ajax/libs/axios/0.18.0/axios.min.js',
+          // element-ui js
+          'https://cdn.bootcdn.net/ajax/libs/element-ui/2.4.5/index.js'
+        ]
+      }
+      config.plugin('html')
+        .tap(args => {
+          args[0].cdn = cdn
+          return args
+        })
     }
   }
 }
